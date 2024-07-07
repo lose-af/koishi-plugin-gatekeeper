43c71c8cc621bfb67c5c055dee16757a80606ae2
Nixieboluo <me@sagirii.me>
2024-07-07 17:12:36 +0800

    fix: try to fix config schema (again)

--- src/config.ts
+++ src/config.ts
@@ -19,25 +19,19 @@ export interface Config {
 }
 
 export const Config: Schema<Config> = Schema.object({
-  identifier: Schema.string().required(),
-  platform: Schema.string().required().default("onebot"),
-  genTicketIn: Schema.string().required(),
-  useTicketIn: Schema.string().required(),
-  validForSeconds: Schema.number().required().default(172800),
-  denyCooldownSeconds: Schema.number().required().default(1314000),
-  removeAfterAccepted: Schema.boolean().required().default(false),
+  identifier: Schema.string(),
+  platform: Schema.string().default("onebot"),
+  genTicketIn: Schema.string(),
+  useTicketIn: Schema.string(),
+  validForSeconds: Schema.number().default(172800),
+  denyCooldownSeconds: Schema.number().default(1314000),
+  removeAfterAccepted: Schema.boolean().default(false),
   message: Schema.object({
-    userAccepted: Schema.string()
-      .required()
-      .default("您的申请已通过, Ticket: {ticket}"),
-    userDenied: Schema.string().required().default("您没有通过审核。"),
-    inDenyCooldown: Schema.string()
-      .required()
-      .default("请待一年冷静期后再次审核。"),
-    noRecord: Schema.string().required().default("进入社区前请先加入审核群。"),
-    invalidTicket: Schema.string().required().default("请正确输入入群凭证。"),
-    expiredTicket: Schema.string()
-      .required()
-      .default("请联系审核群管理员再次发放凭证。"),
+    userAccepted: Schema.string().default("您的申请已通过, Ticket: {ticket}"),
+    userDenied: Schema.string().default("您没有通过审核。"),
+    inDenyCooldown: Schema.string().default("请待一年冷静期后再次审核。"),
+    noRecord: Schema.string().default("进入社区前请先加入审核群。"),
+    invalidTicket: Schema.string().default("请正确输入入群凭证。"),
+    expiredTicket: Schema.string().default("请联系审核群管理员再次发放凭证。"),
   }),
 });
